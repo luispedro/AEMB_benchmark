72dc79c01fd553b691926ee3b39bc2eaacca5bb7
Luis Pedro Coelho <luis@luispedro.org>
2025-05-19 14:31:49 +1000

    RFCT Simplify code

--- CAMI2_running_time.py
+++ CAMI2_running_time.py
@@ -1,63 +1,47 @@
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import pandas as pd
 import numpy as np
 from parse_output import parse_time_output as process_results
+from collections import defaultdict
 
 plt.rcParams['pdf.fonttype'] = 42
 plt.rcParams['ps.fonttype'] = 42
 
-def plot():
-    method_list = ['Bowtie2', 'Bwa', 'Strobealign', 'AEMB']
-    env_result = {'Skin': [], 'Oral': [],
-                  'Airways': [], 'Gastrointestinal': [],
-                  'Urogenital': []}
-    env_list = ['Airways', 'Gastrointestinal', 'Oral',  'Skin','Urogenital',]
-    for env in env_list:
-        print(env)
-        if env == 'Urogenital':
-            num = 9
-        else:
-            num = 10
-        for tool in ['bowtie2', 'bwa', 'strobealign']:
-            running_time = 0
-            running_time += process_results(f'running_time/{env}/{tool}/indexing.txt')[0]
-            for i in range(num):
-                running_time += process_results(f"running_time/{env}/{tool}/map{i+1}.txt")[0]
-                running_time += process_results(f"running_time/{env}/{tool}/sort{i+1}.txt")[0]
-                running_time += process_results(f"running_time/{env}/{tool}/tobam{i+1}.txt")[0]
-                running_time += process_results(f"running_time/{env}/{tool}/cov{i+1}.txt")[0]
-            # print(f"{tool}: {running_time / 60} mins")
-            env_result[env].append(running_time / 60)
+total_time = defaultdict(list)
+envs = ['Airways', 'Gastrointestinal', 'Oral',  'Skin','Urogenital',]
+for env in envs:
+    print(env)
+    for tool in ['bowtie2', 'bwa', 'strobealign', 'seed']:
+        running_time = process_results(f'running_time/{env}/{tool}/indexing.txt').running_time
+        for i in range(9 if env == 'Urogenital' else 10):
+            running_time += process_results(f"running_time/{env}/{tool}/map{i+1}.txt").running_time
+            if tool != 'seed':
+                running_time += process_results(f"running_time/{env}/{tool}/sort{i+1}.txt").running_time
+                running_time += process_results(f"running_time/{env}/{tool}/tobam{i+1}.txt").running_time
+                running_time += process_results(f"running_time/{env}/{tool}/cov{i+1}.txt").running_time
+        total_time[env].append(running_time / 60)
 
-        running_time = 0
-        running_time += process_results(f"running_time/{env}/seed/indexing.txt")[0]
-        for i in range(num):
-            running_time += process_results(f"running_time/{env}/seed/map{i+1}.txt")[0]
-        env_result[env].append(running_time / 60)
-        # print(f"seed: {running_time / 60} mins")
-    print(env_result)
+print(total_time)
 
-    method_list1 = ['Bowtie2', 'Bwa', 'strobealign']
-    for index, method in enumerate(method_list1):
-        print(method)
+for index, method in enumerate(['Bowtie2', 'Bwa', 'strobealign']):
+    print(method)
 
-        time_reduce = []
-        for env in env_list:
-            print(env_result[env][index] - env_result[env][3])
-            print((env_result[env][index] - env_result[env][3]) / env_result[env][index])
-            # num += (env_result[env][index] - env_result[env][3]) / env_result[env][index]
-            time_reduce.append((env_result[env][index] - env_result[env][3]) / env_result[env][index])
-        print(np.min(time_reduce), np.max(time_reduce))
+    time_reduce = []
+    for env in envs:
+        print(total_time[env][index] - total_time[env][3])
+        print((total_time[env][index] - total_time[env][3]) / total_time[env][index])
+        # num += (total_time[env][index] - total_time[env][3]) / total_time[env][index]
+        time_reduce.append((total_time[env][index] - total_time[env][3]) / total_time[env][index])
+    print(np.min(time_reduce), np.max(time_reduce))
 
 
-    subset = pd.DataFrame(np.array([env_result['Airways'],env_result['Gastrointestinal'], env_result['Oral'], env_result['Skin'], env_result['Urogenital']]),
-                          columns = method_list, index=['Airways','Gastrointestinal', 'Oral', 'Skin', 'Urogenital'])
-    print(subset)
-    ax = subset.plot(kind='bar',color = ['#e7298a', '#e6ab02', '#7570b3', '#1b9e77'], figsize=(8,4))
-    ax.set_xticklabels(labels=['Airways','Gastrointestinal', 'Oral', 'Skin', 'Urogenital'], fontsize=10,color = 'black',rotation = 360)
-    ax.set_ylabel('Time (mins)', fontsize=15,color = 'black')
-    plt.savefig(f'CAMI2_running_time.pdf', dpi=300, bbox_inches='tight')
-    plt.close()
+total_time = pd.DataFrame.from_dict(total_time, orient='index', columns=['Bowtie2', 'Bwa', 'Strobealign', 'AEMB']).sort_index()
+print(total_time)
+fig, ax = plt.subplots(figsize=(8,4))
+colors = [cm.Dark2.colors[3], cm.Dark2.colors[5], cm.Dark2.colors[2], cm.Dark2.colors[3]]
+total_time.plot(kind='bar', color=colors, ax=ax)
+ax.set_xticklabels(labels=total_time.index, fontsize=10, color='black', rotation=360)
+ax.set_ylabel('Time (mins)', fontsize=15, color='black')
+fig.savefig(f'plots/CAMI2_running_time.pdf', dpi=300, bbox_inches='tight')
 
-if __name__ == '__main__':
-    plot()
