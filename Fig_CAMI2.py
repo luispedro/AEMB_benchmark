297c69363ba6f79fab8a1b2dd0cfb0bac0574e55
Luis Pedro Coelho <luis@luispedro.org>
2025-05-26 14:19:26 +1000

    ENH Make CAMI2 plots nicer
    Fit the right size &c

--- Fig_CAMI2.py
+++ Fig_CAMI2.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 from collections import defaultdict
 import os
 import pandas as pd
 
-plt.rcParams['pdf.fonttype'] = 42
-plt.rcParams['ps.fonttype'] = 42
+plt.rcParams['svg.fonttype'] = 'none' # Make sure text is not converted to paths in SVG (so they can be edited)
+IN2CM = 2.54
 
 env2indices = {
         'Skin': [1, 13, 14, 15, 16, 17, 18, 19, 20, 28],
@@ -53,9 +54,15 @@ def get_result(env, data_index, amber_path,run_time):
 
     return result
 
+
 def plot_multi_against_multi_hash():
     for env,data_index in env2indices.items():
-        result = {}
+        result = {'strobealign':[0,0,0],
+              'bwa':[0,0,0],
+              'bowtie2':[0,0,0],
+              'seed':[0,0,0],
+              'seed_hash':[0,0,0],
+              'fairy':[0,0,0]}
 
         for run_time in range(5):
             result_run = get_result(env, data_index, f"CAMI2_multi_against_multi/{env}", run_time)
@@ -78,55 +85,40 @@ def plot_multi_against_multi_hash():
 
         plt.legend()
         plt.title(f"{env}", fontsize=20, alpha=1.0, color='black')
-        plt.savefig(f'cami2_short_reads_{env}_multi_against_multi_hash.pdf', dpi=300, bbox_inches='tight')
+        plt.savefig(f'plots/cami2_short_reads_{env}_multi_against_multi_hash.pdf', dpi=300, bbox_inches='tight')
 
-for env, data_index in env2indices.items():
-    result = {'strobealign':[0,0,0],
-              'bwa':[0,0,0],
-              'bowtie2':[0,0,0],
-              'seed':[0,0,0],
-              'fairy':[0,0,0]}
-
+fig,axes = plt.subplots(1, len(env2indices), figsize=(17/IN2CM, 6/IN2CM), sharex=True)
+for ax, (env, data_index) in zip(axes.flat, env2indices.items()):
+    result = pd.DataFrame()
     for run_time in range(5):
-        result_run = get_result(env, data_index, f"CAMI2_multi_against_multi/{env}", run_time)
-        result['strobealign'] = [a + b / 5 for a, b in zip(result['strobealign'], result_run['strobealign'])]
-        result['bwa'] = [a + b / 5 for a, b in zip(result['bwa'], result_run['bwa'])]
-        result['bowtie2'] = [a + b / 5 for a, b in zip(result['bowtie2'], result_run['bowtie2'])]
-        result['seed'] = [a + b / 5 for a, b in zip(result['seed'], result_run['seed'])]
-        result['fairy'] = [a + b / 5 for a, b in zip(result['fairy'], result_run['fairy'])]
+        rt = get_result(env, data_index, f"CAMI2_multi_against_multi/{env}", run_time)
+        result = result.add(pd.DataFrame.from_dict(rt, orient='index', columns=['strain', 'species', 'genus']), fill_value=0)
+    # get the average over the 5 runs
+    result /= 5
 
-    print(result['bwa'])
-    print(result['strobealign'])
-    print(result['bowtie2'])
-    print(result['seed'])
-    print(result['fairy'])
 
     line_width = 1
-    fig,ax = plt.subplots(figsize=(4, 4))
 
-    ax.plot(['genus', 'species', 'strain'],
-             result['strobealign'][::-1], label='strobealign', color='#e7298a',
-             linewidth=line_width, marker='o', )
-
-    ax.plot(['genus', 'species', 'strain'],
-             result['bwa'][::-1], label='BWA', color='#e6ab02',
-             linewidth=line_width, marker='o', )
-
-    ax.plot(['genus', 'species', 'strain'],
-             result['bowtie2'][::-1], label='Bowtie2', color='#7570b3',
-             linewidth=line_width, marker='o', )
-
-    ax.plot(['genus', 'species', 'strain'],
-             result['seed'][::-1], label='AEMB', color='#1b9e77',
-             linewidth=line_width, marker='o', )
+    order = ['genus', 'species', 'strain']
+    tool2color = {
+            'strobealign': '#e7298a',
+            'bwa': '#e6ab02',
+            'bowtie2': '#7570b3',
+            'seed': '#1b9e77',
+            'fairy': '#66a61e'
+            }
+    for tool in ['strobealign', 'bwa', 'bowtie2', 'seed', 'fairy']:
+        ax.plot(order,
+                result.loc[tool, order],
+                label=tool, color=tool2color[tool],
+                linewidth=line_width, marker='o', )
 
 
-    ax.plot(['genus', 'species', 'strain'],
-             result['fairy'][::-1], label='fairy', color='#66a61e',
-             linewidth=line_width, marker='o', )
-
-
-    ax.legend()
-    ax.set_title(f"{env}", fontsize=20, alpha=1.0, color='black')
-    fig.savefig(f'plots/cami2_short_reads_{env}_multi_against_multi_all.pdf', dpi=300, bbox_inches='tight')
+    ax.set_title(f"{env}", alpha=1.0, fontsize=9)
+    ax.set_xticks([0, 1, 2])
+    ax.set_xticklabels(order, rotation=90)
+sns.despine(fig, trim=True)
+fig.tight_layout()
+ax.legend()
+fig.savefig(f'plots/cami2_short_reads.svg', dpi=300, bbox_inches='tight')
 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # AEMB_benchmark
+
+
+- `Fig_running_time_mem_soil.py` : This script generates the running time and memory usage plots for the soil data
+- `Fig_CAMI2.py` : This script generates the CAMI2 plots
+- `Fig_nr_hq_samples.py` : This script generates the number of high-quality samples in the real datasets
