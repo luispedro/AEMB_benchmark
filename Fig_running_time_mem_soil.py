a3742ae375f6618977e211a64d1df3a925ce728b
Luis Pedro Coelho <luis@luispedro.org>
2025-05-27 11:21:51 +1000

    ENH Tighten the plot. Use less space

--- Fig_running_time_mem_soil.py
+++ Fig_running_time_mem_soil.py
@@ -22,9 +22,6 @@ for index in [24, 25, 26, 27, 28, 29, 30, 'hash']:
     if index == 'hash':
         basedir = "running_time/soil/seed_hash"
         method_names.append(f'AEMB(hash)')
-    elif index == 'default':
-        basedir = "running_time/soil/seed"
-        method_names.append(f'AEMB(default)')
     else:
         basedir = f"running_time/soil/seed_N/{index}"
         method_names.append(f'B = {index}')
@@ -40,21 +37,23 @@ for index in [24, 25, 26, 27, 28, 29, 30, 'hash']:
         if time_out.memory_usage > memory_usage:
             memory_usage = time_out.memory_usage
     running_times.append((total_running_time - index_running_time * 10) / 60)
-    memory.append(memory_usage)
-memory = [m / 1024 for m in memory]  # Convert memory from MB to GB
+    memory.append(memory_usage / 1024)  # Convert memory from MB to GB
 
-fig, (ax_rt, ax_mem) = plt.subplots(1,2, figsize=(10/IN2CM, 7/IN2CM))
-bars = ax_rt.bar(method_names, running_times, color=cm.Dark2.colors)
+fig, (ax_rt, ax_mem) = plt.subplots(1,2, figsize=(9/IN2CM, 7/IN2CM))
 
-ax_rt.set_ylabel('Running time (mins)')
-bars = ax_mem.bar(method_names, memory, color=cm.Dark2.colors)
+ax_rt.bar(method_names, running_times, color=cm.Dark2.colors[1], width=0.8, align='center')
+ax_mem.bar(method_names, memory, color=cm.Dark2.colors[5], width=0.8, align='center')
 
-ax_mem.set_ylabel('Memory usage (peak, GB)')
-# rotate x-axis labels
 ax_rt.set_xticks(range(len(method_names)))
-ax_rt.set_xticklabels(method_names, rotation=90, ha='center', fontsize=9)
+ells = ax_rt.set_xticklabels(method_names, rotation=90, ha='center', fontsize=7, va='bottom')
 ax_mem.set_xticks(range(len(method_names)))
-ax_mem.set_xticklabels(method_names, rotation=90, ha='center', fontsize=9)
-fig.tight_layout()
+ax_mem.set_xticklabels(method_names, rotation=90, ha='center', fontsize=7, va='bottom')
+for x in ax_rt.get_xticklabels():
+    x.set_y(0.1)
+for x in ax_mem.get_xticklabels():
+    x.set_y(0.1)
 sns.despine(fig, trim=True)
+fig.tight_layout()
+ax_rt.set_ylabel('Running time (mins)', fontsize=9)
+ax_mem.set_ylabel('Memory usage (peak, GB)', fontsize=9)
 fig.savefig(f'plots/Fig_run_time_mem.svg', dpi=300, bbox_inches='tight')
