94197973a7d2ff9fa40191a7b7e1da1c2658ebd7
Luis Pedro Coelho <luis@luispedro.org>
2025-05-27 14:14:14 +1000

    ENH Add horizontal version of plot

--- Fig_CAMI2_running_time.py
+++ Fig_CAMI2_running_time.py
@@ -9,6 +9,8 @@ total_time = defaultdict(list)
 plt.rcParams['svg.fonttype'] = 'none'
 IN2CM = 2.54
 
+IS_HORIZONTAL = False
+
 
 envs = ['Airways', 'Gastrointestinal', 'Oral',  'Skin','Urogenital',]
 for env in envs:
@@ -27,11 +29,19 @@ total_time = pd.DataFrame.from_dict(total_time, orient='index', columns=['Bowtie
 fig, ax = plt.subplots(figsize=(8/IN2CM,4))
 colors = [cm.Dark2.colors[3], cm.Dark2.colors[5], cm.Dark2.colors[2], cm.Dark2.colors[0]]
 data = pd.melt(total_time.reset_index(), id_vars='index', var_name='Method', value_name='Time (mins)')
-sns.barplot(data=data, hue='Method', x='index', y='Time (mins)', palette=colors, ax=ax, width=0.8, edgecolor='black', linewidth=0.5)
-ax.set_xticks(ticks=range(len(total_time.index)))
-ax.set_xticklabels(labels=total_time.index, fontsize=9, color='black', rotation=90)
-ax.set_ylabel('Time (mins)', fontsize=9, color='black')
-ax.set_xlabel(None)
+if IS_HORIZONTAL:
+    sns.barplot(data=data, hue='Method', y='index', x='Time (mins)', palette=colors, ax=ax, width=0.8, edgecolor='black', linewidth=0.5)
+    ax.set_yticks(ticks=range(len(total_time.index)))
+    ax.set_yticklabels(labels=total_time.index, fontsize=9, color='black', rotation=0)
+    ax.set_xlabel('Time (mins)', fontsize=9, color='black')
+    ax.set_ylabel(None)
+else:
+    sns.barplot(data=data, hue='Method', x='index', y='Time (mins)', palette=colors, ax=ax, width=0.8, edgecolor='black', linewidth=0.5)
+    ax.set_xticks(ticks=range(len(total_time.index)))
+    ax.set_xticklabels(labels=total_time.index, fontsize=9, color='black', rotation=0)
+    ax.set_ylabel('Time (mins)', fontsize=9, color='black')
+    ax.set_xlabel(None)
+
 sns.despine(fig, trim=True)
 fig.tight_layout()
 fig.savefig(f'plots/CAMI2_running_time.svg', dpi=300, bbox_inches='tight')
